f77d340d34f62662658f2175307ac4a02b86ad02
Yusif Osmanov <yusifosmanov475@gmail.com>
2021-11-09 15:52:21 +0400

    excel to db

--- exceltodb.py
+++ exceltodb.py
@@ -10,6 +10,7 @@ UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = b'_4#y2L"F4Q8z\n\xec]/'
 
+engine = create_engine("sqlite:///data.db", echo=False)
 
 @app.route("/", methods=["GET","POST"])
 def func():
@@ -17,21 +18,13 @@ def func():
         file = request.files['file']
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        df = pd.read_excel(file,None)
-        # print(df)
-        a = df.values()
-        # print(a)
-        # print(df.values())
-        def foo(df):
-            list1 = []
-            for j in df.keys():
-                list1.append(j)
-                return list1
-        engine = create_engine("sqlite:///data.db", echo=False)
-        for i in a:
-            print(i)
-            df = pd.read_excel(file,None)
-            i.to_sql(foo(df),con=engine, if_exists="append", index=False)
+        xls = pd.ExcelFile(file)
+        print(xls.sheet_names)
+        for sheet_name in xls.sheet_names:
+            print(sheet_name)
+            sql_tables = sheet_name
+            df = pd.read_excel(file,sheet_name=sheet_name, index_col=None)
+            df.to_sql(sql_tables, con=engine, if_exists="append", index=False)
         return redirect('/')
     return render_template("index.html")
     
